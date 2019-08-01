97c3573c1ccd898d765874f2d02f6085c346ebf0
wulei <wulei2010@qq.com>
2019-08-01 10:11:55 +0800

    修改项目部署线程报错

--- src/main/java/com/example/springboottest/test/TestDemo.java
+++ src/main/java/com/example/springboottest/test/TestDemo.java
@@ -6,7 +6,7 @@ import org.springframework.scheduling.annotation.Async;
 public class TestDemo {
 
     //@Test
-    @Async("getAsyncExecutor")
+    /*@Async("getAsyncExecutor")*/
     public void test1(){
         long l = Long.valueOf("100000000000");
          for(long i = 0; i < l;i++){
