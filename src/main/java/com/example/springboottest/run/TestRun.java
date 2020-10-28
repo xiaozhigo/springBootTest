d2b5b84669420a5a54fdc3c9e2f736e4109f5205
wulei <wulei@erichufund.com>
2020-10-28 10:16:55 +0800

    添加布隆过滤器

--- src/main/java/com/example/springboottest/run/TestRun.java
+++ src/main/java/com/example/springboottest/run/TestRun.java
@@ -9,6 +9,9 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
+/**
+ * 启动
+ */
 @Component
 @Order(-2147483647)
 @Slf4j
