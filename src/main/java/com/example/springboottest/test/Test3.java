5b092eae7778ee2d19f4b80fa0bebe8a7bc22687
wulei <wulei@erichufund.com>
2021-03-03 18:01:18 +0800

    提交

--- src/main/java/com/example/springboottest/test/Test3.java
+++ src/main/java/com/example/springboottest/test/Test3.java
@@ -2,7 +2,6 @@ package com.example.springboottest.test;
 
 import com.example.springboottest.dto.KeyValueModel;
 import com.example.springboottest.dto.UserDetailDto;
-import com.example.springboottest.dto.UserDto;
 import com.example.springboottest.service.TestService;
 import org.junit.Test;
 
@@ -11,13 +10,14 @@ import java.lang.reflect.Method;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
+import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Collectors;
 
 public class Test3 {
 
     @Test
-    public void test1(){
+    public void test1() {
         DateTimeFormatter yyyyMMdd = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
         LocalDateTime now = LocalDateTime.now();
         System.out.println(now);
@@ -25,13 +25,13 @@ public class Test3 {
     }
 
     @Test
-    public void test2(){
+    public void test2() {
         try {
             //Class<?> aClass = Class.forName("com.example.springboottest.service.TestService");
             Class<TestService> aClass = TestService.class;
             Date date = new Date();
-            Method getTableAndTime = aClass.getMethod("getTableAndTime",date.getClass());
-            Object invoke = getTableAndTime.invoke(aClass.newInstance(),date);
+            Method getTableAndTime = aClass.getMethod("getTableAndTime", date.getClass());
+            Object invoke = getTableAndTime.invoke(aClass.newInstance(), date);
             System.out.println(invoke);
         } catch (Exception e) {
             e.printStackTrace();
@@ -39,20 +39,20 @@ public class Test3 {
     }
 
     @Test
-    public void test3(){
+    public void test3() {
         Test1 test1 = SingleDemo.getTest1();
         test1.test3();
     }
 
     @Test
-    public void test4(){
+    public void test4() {
         Test1 test1 = SingleDemo2.getTest1();
         test1.test1();
     }
 
 
     @Test
-    public void test5(){
+    public void test5() {
         List<KeyValueModel<Object>> list = new ArrayList<>();
         KeyValueModel<Object> valueModel = new KeyValueModel<>();
         valueModel.setKey("id");
@@ -67,24 +67,47 @@ public class Test3 {
     }
 
     @Test
-    public void test6(){
+    public void test6() {
         List<Integer> list = new ArrayList<>();
         list.add(1);
         list.add(2);
         list.add(3);
-        List<Integer> collect = list.stream().filter(str -> str==1).collect(Collectors.toList());
+        List<Integer> collect = list.stream().filter(str -> str == 1).collect(Collectors.toList());
         System.out.println(collect);
     }
 
     @Test
-    public void test7(){
+    public void test7() {
         List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
         List<Integer> collect = list.parallelStream().map(i -> i * 2).collect(Collectors.toList());
         System.out.println(collect);
     }
 
     @Test
-    public void test8(){
+    public void test8() {
+        CountDownLatch countDownLatch = new CountDownLatch(3);
+        ExecutorService service = Executors.newFixedThreadPool(3);
+        for (int i = 0; i < 3; i++) {
+            Runnable runnable = new Runnable() {
+                @Override
+                public void run() {
+                    countDownLatch.countDown();
+                    System.out.println(Thread.currentThread().getName() + "开始");
+                }
+            };
+            service.execute(runnable);
+        }
+        try {
+            System.out.println(Thread.currentThread().getName() + "开始等待");
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(Thread.currentThread().getName() + "结束");
+    }
+
+    @Test
+    public void test9() {
         Optional<Object> optional = Optional.ofNullable(null);
         Object o = optional.orElse(0);
         Object elseGet = optional.orElseGet(() -> 1);
@@ -95,24 +118,73 @@ public class Test3 {
     }
 
     @Test
-    public void test9(){
+    public void test10() {
         Optional<Object> optional = Optional.ofNullable(1);
         Object aThrow = optional.orElseThrow(() -> new IllegalArgumentException());
         System.out.println(aThrow);
     }
 
     @Test
-    public void test10(){
-        UserDetailDto userDto = new UserDetailDto("tomcat","cat",null);
+    public void test11() {
+        UserDetailDto userDto = new UserDetailDto("tomcat", "cat", null);
         Integer integer = Optional.ofNullable(userDto).map(u -> u.getUserId()).orElse(2);
         System.out.println(integer);
     }
 
     @Test
-    public void test11(){
-        UserDetailDto userDetailDto = new UserDetailDto("tomcat","cat",null);
+    public void test12() {
+        UserDetailDto userDetailDto = new UserDetailDto("tomcat", "cat", null);
         Optional<UserDetailDto> result = Optional.ofNullable(userDetailDto).filter(u -> u.getUserId() != null && u.getDetailName().contains("c"));
         System.out.println(result.isPresent());
         System.out.println(result);
+    }
+
+    @Test
+    public void test13() {
+        BlockingQueue blockingQueue = new ArrayBlockingQueue<String>(1);
+        blockingQueue.add("1");
+        System.out.println(blockingQueue);
+        blockingQueue.remove();
+        System.out.println(blockingQueue);
+        blockingQueue.remove();
+    }
+
+    @Test
+    public void test14() {
+        BlockingQueue queue = new ArrayBlockingQueue<String>(1);
+        queue.offer("1");
+        System.out.println(queue);
+        queue.poll();
+        System.out.println(queue);
+        queue.poll();
+    }
+
+    @Test
+    public void test15() {
+        BlockingQueue queue = new ArrayBlockingQueue<String>(1);
+        try {
+            queue.put("1");
+            System.out.println(queue);
+            queue.take();
+            System.out.println(queue);
+            queue.take();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void test16() {
+        int processors = Runtime.getRuntime().availableProcessors();
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(processors, processors * 2, 1L, TimeUnit.SECONDS, new ArrayBlockingQueue<Runnable>(20), new ThreadPoolExecutor.CallerRunsPolicy());
+        for (int i = 0; i < 5; i++) {
+            executor.execute(new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println("????");
+                }
+            });
+        }
+        executor.shutdown();
     }
 }
