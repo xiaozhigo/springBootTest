a952336de53f78e77e60f406f56ce5025e35784f
wulei <wulei2010@qq.com>
2021-05-11 21:52:59 +0800

    处理启动报错

--- src/main/java/com/example/springboottest/controller/WebFluxController.java
+++ src/main/java/com/example/springboottest/controller/WebFluxController.java
@@ -3,12 +3,14 @@ package com.example.springboottest.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import reactor.core.publisher.Mono;
 
 @Controller
 public class WebFluxController {
 
     @RequestMapping(value = "webFluxTest1",method = RequestMethod.GET)
+    @ResponseBody
     public Mono<String> test(){
         return Mono.just("哈哈");
     }
